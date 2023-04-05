8f1bd507cee733e2aa7a64461be474abf41b7c2d
cybersosa <129995320+cybersosa@users.noreply.github.com>
2023-04-05 15:52:39 -0400

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -8,12 +8,13 @@ import java.util.Set;
 
 public class FileManager
 {
+    // Define file permissions as a set of PosixFilePermission Objects
     private static final Set<PosixFilePermission> READ_ONLY = PosixFilePermissions.fromString("r--r--r--");
     private static final Set<PosixFilePermission> READ_WRITE = PosixFilePermissions.fromString("rw-rw-rw-");
     private static final Set<PosixFilePermission> EXECUTE_ONLY = PosixFilePermissions.fromString("--x--x--x");
 
 
-
+    // Define a method to set file permissions for a specific file
     public void setFilePermissions(String filename, Set<PosixFilePermission>permissions)
     throws IOException
     {
@@ -21,14 +22,17 @@ public class FileManager
         Files.setPosixFilePermissions(file,permissions);
 
     }
-
+    //Example Usage 
     public static void main(String[] args)
     {
         FileManager fileManager = new FileManager();
 
         try{
+            // Set the permissions of the file "myfile.txt" to READ_ONLY
             fileManager.setFilePermissions("myfile.txt", READ_ONLY);
-            fileManager.setFilePermissions("myapp.sh", EXECUTE_ONLY);
+            
+            // Set the permissions of the file "helloworld.sh" to EXECUTE_ONLY
+            fileManager.setFilePermissions("helloworld.sh", EXECUTE_ONLY);
         }
         catch(IOException e){
             System.err.println("Error setting file permissions:" + e.getMessage());
